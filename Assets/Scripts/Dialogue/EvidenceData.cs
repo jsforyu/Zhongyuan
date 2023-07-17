6b2b1ea72142784f6a2dfe1299dd5260b6e6bbec
cxw299 <747502532@qq.com>
2023-07-17 17:07:47 +0800

    证言UI

--- Assets/Scripts/Dialogue/EvidenceData.cs
+++ Assets/Scripts/Dialogue/EvidenceData.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EvidenceData_So", menuName = "EvidenceData")]
 public class EvidenceData : ScriptableObject
 {
-    public string[,] Evidences = new string[99, 99];
+    //public string[,] Evidences = new string[99, 99];
+    public List<string> npcName=new List<string>();
 
+    public List<string> evidence=new List<string>();
 }
