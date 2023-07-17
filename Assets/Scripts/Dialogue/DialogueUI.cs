3572b9363a9192bf1d7b905d194bf1acbf7da837
YUyu <71108328+jsforyu@users.noreply.github.com>
2023-07-17 15:18:34 +0800

    1

--- Assets/Scripts/Dialogue/DialogueUI.cs
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -17,7 +17,6 @@ public class DialogueUI : MonoBehaviour
     public GameObject QuestionButton;
     public Animator fadeText;
     public string[,] Evidences = new string[99,99];
-    public List<ItemDataSO> clews;
     GameObject currentcharacter;
     int index;
     int eindex;
@@ -79,7 +78,8 @@ public class DialogueUI : MonoBehaviour
         Content.text = "";
         textfinshed = false;
         int clewindex=0;
-        if (currentcharacter.GetComponent<Character>().dialogue.DialogueList[index].clewitem !=null) clews.Add(currentcharacter.GetComponent<Character>().dialogue.DialogueList[index].clewitem);
+        if (currentcharacter.GetComponent<Character>().dialogue.DialogueList[index].clewitem !=null)
+            BagController.Instance.BagAddItem(currentcharacter.GetComponent<Character>().dialogue.DialogueList[index].clewitem);
         for (int i = 0; i < currentcharacter.GetComponent<Character>().dialogue.DialogueList[index].dialoguetext.Length; i++)
         {
             if ((clewindex< currentcharacter.GetComponent<Character>().dialogue.DialogueList[index].clew.Length) &&   
--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public ItemDataSO ItemData;
+    public int itemstate;//0为可以调查，1为不可以调查
     private void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(0))
         {
             BagController.Instance.BagAddItem(ItemData);
             Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (itemstate == 0)
+        {
+
         }
     }
 }
