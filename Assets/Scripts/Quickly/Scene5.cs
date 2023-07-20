95d10409155c0ae2f20ac5c823d142fbf7e90ca7
cxw299 <747502532@qq.com>
2023-07-20 13:58:34 +0800

    场景五

---
+++ Assets/Scripts/Quickly/Scene5.cs
@@ -0,0 +1,74 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scene5 : MonoBehaviour
+{
+    [SerializeField] private DialogueData_So dialogueData_So;
+
+    [SerializeField] private BagDataSO bag;
+
+    [SerializeField] private Text npcName;
+
+    [SerializeField] private Text dialogue;
+
+    [SerializeField] private GameObject evidence;
+
+    private AudioSource audioSource;
+    private int index = -1;
+
+    private bool isok = false;
+
+    private float showtime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+             if(Input.GetMouseButton(0)&&isok)
+         {
+            evidence.SetActive(false);
+            index++;
+            isok = false;
+            showtime = 0;
+            if (index >= dialogueData_So.DialogueList.Count)
+            {
+                int temp = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(temp+1);
+            }
+            else
+            {
+                dialogue.text = "";
+                npcName.text = dialogueData_So.DialogueList[index].npcName;
+                dialogue.DOText(dialogueData_So.DialogueList[index].dialoguetext, 1f);
+                if (dialogueData_So.DialogueList[index].clewitem !=null)
+                {
+                    audioSource.Play();
+                    for(int i=0;i<bag.itemData.Count;i++)
+                    {
+                        if (bag.itemData[i]==null)
+                        {
+                            bag.itemData[i] = dialogueData_So.DialogueList[index].clewitem;
+                            evidence.SetActive(true);
+                            break;
+                        }
+                    }
+                }
+            }
+         }
+        showtime += Time.deltaTime;
+        if(showtime>=2)
+        {
+            showtime = 0;
+            isok = true;
+        }
+    }
+}
