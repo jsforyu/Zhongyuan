ede674f37a9468859e629f6b4ce6a2899bc191c6
YUyu <71108328+jsforyu@users.noreply.github.com>
2023-07-21 21:29:30 +0800

    1

--- Assets/Scripts/Manager/PanelManager.cs
+++ Assets/Scripts/Manager/PanelManager.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class PanelManager : MonoBehaviour
 {
     public GameObject dialogue;
+
+    private void Start()
+    {
+        Player.instance.state = 1;
+    }
+
     public void ShowUI()
     {
         this.gameObject.SetActive(false);
         dialogue.SetActive(true);
         DialogueUI.instance.ShowDialogueUI();
+    }
+
+    public void ShowUI2()
+    {
+        this.gameObject.SetActive(false);
     }
 }
